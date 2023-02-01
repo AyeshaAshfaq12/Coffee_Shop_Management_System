a559c746106c0cbac19e53f9275e2479a875225a
AyeshaAshfaq12 <ayeshaashfaq925@gmail.com>
2023-02-01 15:48:10 +0500

    Create MenuItem.cs

---
+++ problem1/problem1/BL/MenuItem.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace problem1.BL
+{
+    class MenuItem
+    {
+
+        public string name;
+        public string type;
+        public int price;
+
+        public MenuItem(string name, string type, int price)
+        {
+            this.name = name;
+            this.type = type;
+            this.price = price;
+        }
+
+    }
+}
